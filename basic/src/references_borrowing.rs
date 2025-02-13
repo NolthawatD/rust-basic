fe2725c071f5e7f23d02b99a7b49965dca71cc58
Nolthawat Deangharn <nolthawat.d@gmail.com>
2025-02-13 20:58:52 +0700

    fix: reference to print

--- basic/src/references_borrowing.rs
+++ basic/src/references_borrowing.rs
@@ -4,7 +4,7 @@ pub fn test1() {
 	println!("\n\nReference and Borrowing");
 
 	let s1 = String::from("abc");
-	let s2 = &s1; //Borrowing s1 by reference from s1
-	// drop(s2);
-	drop(s1); // จะสามารถ drop ได้ เมื่อ reference เท่านั้น
+	let s2 = &s1; // Borrowing s1 by reference from s1
+	println!("{}", s2);
+	println!("{}", s1); // จะสามารถ print ได้ เมื่อถูก reference เท่านั้น
 }
